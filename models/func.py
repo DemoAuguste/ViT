b5bb223cab047dcd971d66a53bacabcc379e2441
DemoAuguste <393493772@qq.com>
2021-07-01 20:40:35 +0800

    Update func.py

--- models/func.py
+++ models/func.py
@@ -22,10 +22,10 @@ def get_models(args):
         if args.arch.lower() == "vit": # add vision transformer
             if args.pretrained:
                 print("=> using pre-trained model '{}'".format(args.arch))
-                model = ViT('B_16_imagenet1k', pretrained=True, image_size=224)
+                model = ViT('B_16', pretrained=True)
             else:
                 print("=> creating model '{}'".format(args.arch))
-                model = ViT('B_16_imagenet1k', pretrained=False, image_size=224)
+                model = ViT('B_16', pretrained=False)
 
             # model = ViT(image_size=args.image_size,
             #             patch_size=args.patch_size,
